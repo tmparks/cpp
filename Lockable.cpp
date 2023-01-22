7d4c571d7b38e14a6ec957ed4ccd7a927b26ccb5
Tom Parks <tmparks@yahoo.com>
2023-01-22 16:12:30 +0000

    lockable object where ownership can be tested

---
+++ Lockable.cpp
@@ -0,0 +1,83 @@
+#include "Verbose.hpp"
+
+#include <gsl/gsl>
+
+#include <atomic>
+#include <cassert>
+#include <mutex>
+#include <thread>
+
+// See https://en.cppreference.com/w/cpp/named_req/BasicLockable
+class BasicLockable : protected Verbose {
+public:
+  BasicLockable() : Verbose {gsl::czstring(__func__)} {}
+
+  void lock() {
+    mutex_.lock();
+    owner_ = std::this_thread::get_id();
+  }
+
+  void unlock() {
+    owner_ = none_;
+    mutex_.unlock();
+  }
+
+  void assert_ownership() {
+    assert((owner_ == std::this_thread::get_id()) &&
+           "Lock is owned by this thread");
+  }
+
+  void this_thread_owns_lock() {
+#ifndef NDEBUG
+    if (owner_ != std::this_thread::get_id()) {
+      throw std::runtime_error("Lock is not owned by this thread!");
+    }
+#endif
+  }
+
+private:
+  static const std::thread::id none_; // id that is not associated with any thread
+  std::atomic<std::thread::id> owner_ {none_}; // initially unowned
+  std::mutex mutex_; // initially unlocked
+};
+
+const std::thread::id BasicLockable::none_ {};
+
+////////////////////////////////////////////////////////////////////////////////
+
+#include <gtest/gtest.h>
+#include <iostream>
+
+using namespace testing;
+using namespace testing::internal;
+
+TEST(BasicLockable, lock_guard) {
+  auto mutex = BasicLockable();
+  auto lock = std::lock_guard<decltype(mutex)>(mutex);
+  mutex.assert_ownership();
+}
+
+TEST(BasicLockable, unique_lock) {
+  auto mutex = BasicLockable();
+  auto lock = std::unique_lock<decltype(mutex)>(mutex);
+  mutex.assert_ownership();
+}
+
+TEST(BasicLockable, unowned_throw) {
+  auto mutex = BasicLockable();
+#ifdef NDEBUG
+  EXPECT_NO_THROW(mutex.this_thread_owns_lock());
+#else
+  EXPECT_THROW(mutex.this_thread_owns_lock(), std::runtime_error);
+#endif // NDEBUG
+}
+
+TEST(BasicLockable, DISABLED_unowned_throw_unhandled) {
+  auto mutex = BasicLockable();
+  mutex.this_thread_owns_lock();
+}
+
+TEST(BasicLockable, DISABLED_unowned_assert) {
+  auto mutex = BasicLockable();
+  mutex.assert_ownership(); // Expected to fail!
+}
