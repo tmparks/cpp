f76b8638d575d75c2a6eb764899405f23b9da175
Tom Parks <tmparks@yahoo.com>
2024-04-05 12:23:36 +0000

    rule of zero

--- Gadget.hpp
+++ Gadget.hpp
@@ -3,22 +3,12 @@
 
 // Derived publicly (and indirectly) from std::enable_shared_from_this
 class Gadget : public Widget {
-public:
-    ~Gadget() override = default;
-
 public: // pseudo-protected
     explicit Gadget(Protected);
     Gadget(Protected, const Gadget& w);
 
 protected:
     Gadget(const std::string& name);
-    Gadget(const Gadget&) = default;
-
-    // Rule of five.
-    Gadget() = delete;
-    Gadget(Gadget&&) = delete;
-    Gadget& operator=(const Gadget&) = delete;
-    Gadget& operator=(Gadget&&) = delete;
 
 private:
     std::shared_ptr<AbstractWidget> cloneImpl() const override;
--- Widget.hpp
+++ Widget.hpp
@@ -3,22 +3,12 @@
 
 // Derived publicly (and indirectly) from std::enable_shared_from_this
 class Widget : public AbstractWidget {
-public:
-    ~Widget() override = default;
-
 public: // pseudo-protected
     explicit Widget(Protected);
     Widget(Protected, const Widget& w);
 
 protected:
     Widget(const std::string& name);
-    Widget(const Widget&) = default;
-
-    // Rule of five.
-    Widget() = delete;
-    Widget(Widget&&) = delete;
-    Widget& operator=(const Widget&) = delete;
-    Widget& operator=(Widget&&) = delete;
 
 private:
     std::shared_ptr<AbstractWidget> cloneImpl() const override;
