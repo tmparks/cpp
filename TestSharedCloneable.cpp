0e5a42888e9b90399fd40755fa56cb9fb253b9c9
Tom Parks <tmparks@yahoo.com>
2024-05-24 12:22:24 +0000

    verify -Werror

--- TestSharedCloneable.cpp
+++ TestSharedCloneable.cpp
@@ -35,6 +35,7 @@ protected:
 };
 
 TEST(SharedCloneable, clone) {
+    int unused = 0; // -Werror
     auto p1 = create<ConcreteObject>();
     auto p2 = p1->shared_from_this(); // copy of pointer
     auto p3 = p1->clone();
