f694316b05fa24bc82d5110ea661b909ce84eb87
Tom Parks <tmparks@yahoo.com>
2024-09-12 23:11:50 +0000

    explain examples

--- TestStrongType.cpp
+++ TestStrongType.cpp
@@ -3,45 +3,67 @@
 
 #include <gtest/gtest.h>
 
-// Use opaque scoped enumerations for strong identifier types.
-enum class RedId : uintmax_t { invalid = 0 };
-enum class BlueId : uintmax_t { invalid = 0 };
+// Good: Use simple struct for strong type, comment expresses intent.
+struct Mass {
+    double asDouble{0.0};
+}; // Strong type.
 
-// Generate the next ID.
-template <typename E>
-E next() {
-    // Hide unsightly casts inside a function.
-    static auto previous = static_cast<std::underlying_type<E>::type>(E::invalid);
-    return static_cast<E>(++previous);
-}
-
-// Use simple struct for a generic strong type.
+// Better: Use inheritance so that code expresses intent.
+// [Express ideas directly in code](https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#p1-express-ideas-directly-in-code)
 struct Length : StrongType<double> { };
 struct Width : Length { };
 struct Height : Length { };
 struct Area : StrongType<double> { };
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Bad: Use opaque scoped enum for strong identifier type, comment expresses intent.
+enum class RedId : uintmax_t { null = 0 }; // Strong identifier.
+
+// Bad: Use MACRO because scoped enum does not allow inheritance.
+// [Don’t use macros for constants or “functions”](https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#es31-dont-use-macros-for-constants-or-functions)
+#define STRONG_IDENTIFIER(type) enum class type : uintmax_t { null = 0 }
+
+STRONG_IDENTIFIER(BlueId);
+
+// Generate the next ID.
+template <typename E>
+typename std::enable_if<std::is_enum<E>::value, E>::type next() {
+    static auto previous = static_cast<std::underlying_type<E>::type>(E::null);
+    return static_cast<E>(++previous);
+}
+
+// Better: Use inheritance so that code expresses intent.
+// [Express ideas directly in code](https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#p1-express-ideas-directly-in-code)
+struct GreenId : StrongIdentifier<> { };
+
+// Generate the next ID.
+template <typename T>
+typename std::enable_if<not std::is_enum<T>::value, T>::type next() {
+    static auto previous = T::null;
+    return {++previous};
+}
 
 STRONG_FLOATING_POINT_OPERATORS(Length)
 
 TEST(StrongType, id) {
     auto red = RedId{1};
     auto rojo = RedId{1};
-    auto rouge = RedId::invalid;
+    auto rouge = RedId::null;
     auto blue = BlueId{2};
     auto azul = BlueId{2};
-    auto bleu = BlueId::invalid;
+    auto bleu = BlueId::null;
 
     EXPECT_EQ(red, rojo);
     EXPECT_EQ(blue, azul);
 
-    EXPECT_NE(red, RedId::invalid);
-    EXPECT_NE(rojo, RedId::invalid);
-    EXPECT_EQ(rouge, RedId::invalid);
+    EXPECT_NE(red, RedId::null);
+    EXPECT_NE(rojo, RedId::null);
+    EXPECT_EQ(rouge, RedId::null);
 
-    EXPECT_NE(blue, BlueId::invalid);
-    EXPECT_NE(azul, BlueId::invalid);
-    EXPECT_EQ(bleu, BlueId::invalid);
-    // EXPECT_EQ(bleu, invalid); // compile time error
+    EXPECT_NE(blue, BlueId::null);
+    EXPECT_NE(azul, BlueId::null);
+    EXPECT_EQ(bleu, BlueId::null);
 
     EXPECT_EQ(red, next<RedId>()); // 1
     EXPECT_NE(red, next<RedId>()); // 2
@@ -50,9 +72,9 @@ TEST(StrongType, id) {
     EXPECT_EQ(blue, next<BlueId>()); // 2
 
     // Cannot compare different types.
-    // EXPECT_NE(RedId::invalid, BlueId::invalid); // compile time error
+    // EXPECT_EQ(RedId::null, BlueId::null); // compile time error
     // EXPECT_NE(red, blue); // compile time error
-    // EXPECT_NE(blue, other); // compile time error
+    // EXPECT_EQ(blue, 2); // compile time error
 }
 
 TEST(StrongType, initialization) {
