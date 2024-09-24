60a9dc10262e5f71eacd6178be130dd7447642b4
Tom Parks <tmparks@yahoo.com>
2024-09-24 11:55:48 +0000

    clang-tidy NOLINT

--- compat/numbers20.hpp
+++ compat/numbers20.hpp
@@ -9,8 +9,15 @@
 #pragma message "emulating C++20 numbers"
 
 #include <cmath>
-namespace std::numbers {
-    constexpr auto pi = M_PI;
-} // namespace std::numbers
+
+// NOLINTBEGIN(*-concat-nested-namespaces) until C++17
+
+namespace std {
+    namespace numbers {
+        constexpr auto pi = M_PI;
+    } // namespace numbers
+} // namespace std
+
+// NOLINTEND(*-concat-nested-namespaces)
 
 #endif // C++20
--- compat/ranges23.hpp
+++ compat/ranges23.hpp
@@ -31,21 +31,29 @@ std::ostream& operator<<(std::ostream& stream, std::tuple<Ts...> const& tuple) {
 #include <boost/tuple/tuple.hpp>
 #include <boost/tuple/tuple_io.hpp>
 
+// NOLINTBEGIN(*-concat-nested-namespaces) until C++17
+
 namespace std {
 
     using boost::get;
 
-    namespace ranges::views {
+    namespace ranges {
 
-        // See [Function Aliases In C++](https://www.fluentcpp.com/2017/10/27/function-aliases-cpp/)
-        template <typename... Args>
-        inline auto zip(Args&&... args)
-                -> decltype(boost::combine(std::forward<Args>(args)...)) {
-            return boost::combine(std::forward<Args>(args)...);
-        }
+        namespace views {
 
-    } // namespace ranges::views
+            // See [Function Aliases In C++](https://www.fluentcpp.com/2017/10/27/function-aliases-cpp/)
+            template <typename... Args>
+            inline auto zip(Args && ... args)
+                    -> decltype(boost::combine(std::forward<Args>(args)...)) {
+                return boost::combine(std::forward<Args>(args)...);
+            }
+
+        } // namespace views
+
+    } // namespace ranges
 
 } // namespace std
+
+// NOLINTEND(*-concat-nested-namespaces)
 
 #endif // C++23
