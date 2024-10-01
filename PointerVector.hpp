f8b29157977ca447ab663ef61ed8d86bca093bc3
Tom Parks <tmparks@yahoo.com>
2024-10-01 23:06:40 +0000

    has_as vs is_a

--- PointerVector.hpp
+++ PointerVector.hpp
@@ -17,6 +17,22 @@ template <typename T>
 using SharedPointerVector = PointerVector<std::shared_ptr<T>>;
 
 template <typename V>
+RawPointerVector<typename V::value_type> reference(V& vector) {
+    auto result = RawPointerVector<typename V::value_type>{};
+    result.reserve(vector.size());
+    for (auto& element : vector) {
+        result.push_pack(&element);
+    }
+    return result;
+}
+
+template <typename V>
+const RawPointerVector<typename V::value_type> reference(const V& vector) {
+    return const_cast<const RawPointerVector<typename V::value_type>>(
+            reference(const_cast<V&>(vector))); // NOLINT(*-const-cast)
+}
+
+template <typename V>
 RawPointerVector<typename V::value_type> mask(V& vector, std::vector<bool>& mask) {
     auto result = RawPointerVector<typename V::value_type>{};
     auto size = std::min(vector.size(), mask.size());
@@ -39,89 +55,115 @@ const RawPointerVector<typename V::value_type> mask(
 ////////////////////////////////////////////////////////////////////////////////
 
 template <typename P>
-class PointerVector : public std::vector<P> {
-private:
-    using base = std::vector<P>;
-
+class PointerVector {
 public:
+    using base_type = typename std::vector<P>;
     using value_type = typename std::pointer_traits<P>::element_type;
-    using size_type = typename base::size_type;
+    using size_type = typename base_type::size_type;
     using reference = value_type&;
     using const_reference = const value_type&;
-    using pointer = P;
     using iterator =
-            boost::indirect_iterator<typename base::iterator>;
+            boost::indirect_iterator<typename base_type::iterator>;
     using const_iterator =
-            boost::indirect_iterator<typename base::const_iterator>;
+            boost::indirect_iterator<typename base_type::const_iterator>;
     using reverse_iterator =
-            boost::indirect_iterator<typename base::reverse_iterator>;
+            boost::indirect_iterator<typename base_type::reverse_iterator>;
     using const_reverse_iterator =
-            boost::indirect_iterator<typename base::const_reverse_iterator>;
+            boost::indirect_iterator<typename base_type::const_reverse_iterator>;
 
-    using base::base;
-    ~PointerVector() = default;
+    // Base access
 
-    [[nodiscard]] reference at(size_type pos) { return *base::at(pos); }
+    base_type base{};
+
+    // Comparison
+
+    bool operator==(const PointerVector& other) const noexcept {
+        return base == other.base;
+    }
+
+    bool operator<(const PointerVector& other) const noexcept {
+        return base < other.base;
+    }
+
+    // Element access
+
+    [[nodiscard]] reference at(size_type pos) { return *base.at(pos); }
+
     [[nodiscard]] const_reference at(size_type pos) const {
-        return *base::at(pos);
+        return *base.at(pos);
     }
 
-    [[nodiscard]] reference operator[](size_type pos) {
-        return *base::operator[](pos);
-    }
+    [[nodiscard]] reference operator[](size_type pos) { return *base[pos]; }
+
     [[nodiscard]] const_reference operator[](size_type pos) const {
-        return *base::operator[](pos);
+        return *base[pos];
     }
 
-    [[nodiscard]] reference front() { return *base::front(); }
-    [[nodiscard]] const_reference front() const { return *base::front(); }
+    [[nodiscard]] reference front() { return *base.front(); }
 
-    [[nodiscard]] reference back() { return *base::back(); }
-    [[nodiscard]] const_reference back() const { return *base::back(); }
+    [[nodiscard]] const_reference front() const { return *base.front(); }
 
-    [[nodiscard]] iterator begin() { return iterator{base::begin()}; }
+    [[nodiscard]] reference back() { return *base.back(); }
+
+    [[nodiscard]] const_reference back() const { return *base.back(); }
+
+    // Iterators
+
+    [[nodiscard]] iterator begin() { return iterator{base.begin()}; }
+
     [[nodiscard]] const_iterator begin() const {
-        return const_iterator{base::begin()};
-    }
-    [[nodiscard]] const_iterator cbegin() const noexcept {
-        return const_iterator{base::cbegin()};
+        return const_iterator{base.begin()};
     }
 
-    [[nodiscard]] iterator end() noexcept { return iterator{base::end()}; }
-    [[nodiscard]] const_iterator end() const noexcept {
-        return const_iterator{base::end()};
+    [[nodiscard]] const_iterator cbegin() const noexcept {
+        return const_iterator{base.cbegin()};
     }
+
+    [[nodiscard]] iterator end() noexcept { return iterator{base.end()}; }
+
+    [[nodiscard]] const_iterator end() const noexcept {
+        return const_iterator{base.end()};
+    }
+
     [[nodiscard]] const_iterator cend() const noexcept {
-        return const_iterator{base::cend()};
+        return const_iterator{base.cend()};
     }
 
     [[nodiscard]] reverse_iterator rbegin() {
-        return reverse_iterator{base::rbegin()};
+        return reverse_iterator{base.rbegin()};
     }
+
     [[nodiscard]] const_reverse_iterator rbegin() const {
-        return const_reverse_iterator{base::rbegin()};
+        return const_reverse_iterator{base.rbegin()};
     }
+
     [[nodiscard]] const_reverse_iterator crbegin() const noexcept {
-        return const_reverse_iterator{base::crbegin()};
+        return const_reverse_iterator{base.crbegin()};
     }
 
     [[nodiscard]] reverse_iterator rend() {
-        return reverse_iterator{base::rend()};
-    }
-    [[nodiscard]] const_reverse_iterator rend() const {
-        return const_reverse_iterator{base::rend()};
-    }
-    [[nodiscard]] const_reverse_iterator crend() const noexcept {
-        return const_reverse_iterator{base::crend()};
+        return reverse_iterator{base.rend()};
     }
 
-private:
-    PointerVector(typename base::size_type, typename base::const_reference);
-    explicit PointerVector(typename base::size_type);
-    typename base::iterator insert(
-            typename base::const_iterator,
-            typename base::size_type,
-            typename base::const_reference);
-    using base::data;
-    using base::resize;
+    [[nodiscard]] const_reverse_iterator rend() const {
+        return const_reverse_iterator{base.rend()};
+    }
+
+    [[nodiscard]] const_reverse_iterator crend() const noexcept {
+        return const_reverse_iterator{base.crend()};
+    }
+
+    // Capacity
+
+    [[nodiscard]] bool empty() const { return base.empty(); }
+
+    [[nodiscard]] size_type size() const { return base.size(); }
+
+    void reserve(size_type cap) { base.reserve(cap); }
+
+    [[nodiscard]] size_type capacity() const { return base.capacity(); }
+
+    // Modifiers
+
+    void clear() { base.clear(); }
 };
--- TestPointerVector.cpp
+++ TestPointerVector.cpp
@@ -21,21 +21,59 @@ namespace {
     }
 } // anonymous namespace
 
-TEST(SharedPointerVector, container) {
+TEST(SharedPointerVector, emplace_back) {
     // Place elements into container as pointers.
     auto container = SharedPointerVector<Verbose>{};
-    container.emplace_back(std::make_shared<Verbose>("one"));
-    container.emplace_back(std::make_shared<Verbose>("two"));
-    container.emplace_back(std::make_shared<Verbose>("three"));
+    auto& base = container.base;
+    base.emplace_back(std::make_shared<Verbose>("one"));
+    base.emplace_back(std::make_shared<Verbose>("two"));
+    base.emplace_back(std::make_shared<Verbose>("three"));
 
     print(container);
+}
 
-    // Access elements of the container as references.
-    for (const auto& elem : container) {
-        std::cout << elem.name() << std::endl;
-    }
+TEST(SharedPointerVector, shallow_copy) {
+    // Place elements into container as pointers.
+    auto container = SharedPointerVector<Verbose>{};
+    auto& base = container.base;
+    base.emplace_back(std::make_shared<Verbose>("one"));
+    base.emplace_back(std::make_shared<Verbose>("two"));
+    base.emplace_back(std::make_shared<Verbose>("three"));
 
     // Copying a container of pointers does not copy the referenced objects.
     auto container_copy = container;
     print(container_copy);
+
+    container_copy[0].name() = "alfa";
+    container_copy[1].name() = "bravo";
+    container_copy[2].name() = "charlie";
+
+    EXPECT_EQ(container, container_copy); // shallow copy
+}
+
+TEST(UniquePointerVector, uncopyable) {
+    auto container = UniquePointerVector<Verbose>{};
+    auto& base = container.base;
+    base.emplace_back(std::make_unique<Verbose>("one"));
+    base.emplace_back(std::make_unique<Verbose>("two"));
+    base.emplace_back(std::make_unique<Verbose>("three"));
+
+    // auto container_copy = container; // compile time error
+}
+
+TEST(SharedPointerVector, erase_remove) {
+    auto container = SharedPointerVector<Verbose>{};
+    auto& base = container.base;
+    base.emplace_back(std::make_shared<Verbose>("one"));
+    base.emplace_back(std::make_shared<Verbose>("two"));
+    base.emplace_back(std::make_shared<Verbose>("three"));
+
+    // In order to avoid copying/moving values,
+    // use the base vector of pointers for erase-remove.
+    auto predicate = [](const std::shared_ptr<Verbose>& v) {
+        return v->name() == "two";
+    };
+    base.erase(std::remove_if(base.begin(), base.end(), predicate), base.end());
+
+    print(container);
 }
