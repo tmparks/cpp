c09cd1f4a96de8873180bcdee53bed58f3e68876
Tom Parks <tmparks@yahoo.com>
2024-10-16 11:55:49 +0000

    segregate shape tests

--- Shape.cpp
+++ Shape.cpp
@@ -1,11 +1,7 @@
 #include "Shape.hpp"
-
 #include "compat/gsl14.hpp"
 #include "compat/numbers20.hpp"
-
-#include <algorithm>
 #include <cmath>
-#include <numbers>
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -206,52 +202,4 @@ Circle::Circle(double radius) : Ellipse { 2 * radius, 2 * radius } { }
 
 Circle* Circle::cloneImpl() const {
     return new Circle { *this }; // NOLINT(*-owning-memory)
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-#include <gtest/gtest.h>
-
-namespace { // anonymous namespace for definitions that are local to this file
-
-    void TestClone(const Shape& original) {
-        std::cout << gsl::czstring { __func__ } << ": "
-                  << typeid(original).name() << std::endl;
-        auto clone = original.clone();
-        EXPECT_NE(clone.get(), &original) << "distinct objects";
-
-        // Necessary (but not sufficient) conditions for two shapes to be the same.
-        EXPECT_DOUBLE_EQ(clone->area(), original.area());
-        EXPECT_DOUBLE_EQ(
-                clone->boundingCircle().area(), original.boundingCircle().area());
-        EXPECT_DOUBLE_EQ(
-                clone->boundingBox().area(), original.boundingBox().area());
-    }
-
-    // NOLINTBEGIN(*-avoid-magic-numbers)
-
-    void TestScale(const Shape& shape) {
-        auto area1 = shape.clone()->scaleTo(1.0).area();
-        auto area2 = shape.clone()->scaleTo(2.0).area();
-        auto area3 = shape.clone()->scaleTo(3.0).scaleBy(4.0).area();
-        EXPECT_DOUBLE_EQ(4.0 * area1, area2);
-        EXPECT_DOUBLE_EQ(36.0 * area2, area3);
-    }
-
-    // NOLINTEND(*-avoid-magic-numbers)
-
-} // anonymous namespace
-
-TEST(Shape, clone) {
-    TestClone(Rectangle(3, 4));
-    TestClone(Square(2));
-    TestClone(Ellipse(3, 4));
-    TestClone(Circle(2));
-}
-
-TEST(Shape, scale) {
-    TestScale(Rectangle(3, 4));
-    TestScale(Square(2));
-    TestScale(Ellipse(3, 4));
-    TestScale(Circle(2));
 }
