956c667191f8833d86a96690bdb8229e01ce78c1
Tom Parks <tmparks@yahoo.com>
2024-10-23 00:19:52 +0000

    propagate_const

--- TestConstPointer.cpp
+++ TestConstPointer.cpp
@@ -1,6 +1,6 @@
-#include "CoQual.hpp"
 #include "Verbose.hpp"
 #include <gmock/gmock.h>
+#include <experimental/propagate_const>
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -9,13 +9,13 @@ using namespace testing;
 using namespace testing::internal;
 
 template <typename T>
-using SharedPtr = CoQual<std::shared_ptr<T>>;
+using SharedPtr = std::experimental::propagate_const<std::shared_ptr<T>>;
 
 template <typename T>
 using SharedVector = std::vector<SharedPtr<T>>;
 
 template <typename T>
-using UniquePtr = CoQual<std::unique_ptr<T>>;
+using UniquePtr = std::experimental::propagate_const<std::unique_ptr<T>>;
 
 template <typename T>
 using UniqueVector = std::vector<UniquePtr<T>>;
@@ -24,19 +24,19 @@ bool isConst(Verbose<>&) { return false; }
 
 bool isConst(const Verbose<>&) { return true; }
 
-TEST(Qual, unqual_shared_ptr) {
+TEST(ConstPointer, mutable_shared_ptr) {
     // const pointer to mutable object
     const auto p = std::make_shared<Verbose<>>("unqual");
     EXPECT_FALSE(isConst(*p));
 }
 
-TEST(Qual, qual_shared_ptr) {
+TEST(ConstPointer, const_shared_ptr) {
     // const pointer to const object
     const auto p = SharedPtr<Verbose<>>{std::make_shared<Verbose<>>("qual")};
     EXPECT_TRUE(isConst(*p));
 }
 
-TEST(Qual, qual_shared_vector) {
+TEST(ConstPointer, const_shared_vector) {
     // const pointers to const objects
     auto v = SharedVector<Verbose<>>{};
     v.emplace_back(std::make_shared<Verbose<>>("one"));
@@ -52,19 +52,19 @@ TEST(Qual, qual_shared_vector) {
     }
 }
 
-TEST(Qual, unqual_unique_ptr) {
+TEST(ConstPointer, mutable_unique_ptr) {
     // const pointer to mutable object
     const auto p = std::make_unique<Verbose<>>("unqual");
     EXPECT_FALSE(isConst(*p));
 }
 
-TEST(Qual, qual_unique_ptr) {
+TEST(ConstPointer, const_unique_ptr) {
     // const pointer to const object
     const auto p = UniquePtr<Verbose<>>(std::make_unique<Verbose<>>("qual"));
     EXPECT_TRUE(isConst(*p));
 }
 
-TEST(Qual, qual_unique_vector) {
+TEST(ConstPointer, const_unique_vector) {
     // const pointers to const objects
     auto v = UniqueVector<Verbose<>>{};
     v.emplace_back(std::make_unique<Verbose<>>("one"));
